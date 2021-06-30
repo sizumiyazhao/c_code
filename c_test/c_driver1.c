bffc09c8f4600cbe7885d172c44d9c1e5777cbed
zhao.jing <290247335@qq.com>
2021-06-30 14:01:43 +0800

    add driver1

---
+++ c_test/c_driver1.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct test_driver
+{
+	int a;
+	void *pri_data;
+	int (*fun)(void *data);
+};
+
+struct private_data
+{
+	int io_num;
+	char name[20];
+};
+
+struct private_data test_data=
+{
+	10,
+	"io_b"
+
+};
+
+int fun_pri(void *data)
+{
+	struct private_data *tmp=data;
+	printf("a is %d,name is %s\n",tmp->io_num,tmp->name);
+	return 1;
+}
+
+struct test_driver driver1
+=
+{
+	11,
+	&test_data,
+	fun_pri
+};
+
+struct test_driver driver2
+=
+{
+	11,
+	&test_data,
+	fun_pri
+};
+
+int main()
+{
+	printf("test\n");
+	driver1.fun(driver1.pri_data);
+    system("pause");
+}
+struct test_driver driver1
+=
+{
+	11,
+	&test_data,
+	fun_pri
+};
+
+struct test_driver driver1
+=
+{
+	11,
+	&test_data,
+	fun_pri
+};
